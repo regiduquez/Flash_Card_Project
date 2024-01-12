29fa3f7363349717b8c2d1e775fff1cd9aaf32b9
regiduquez <regiduquez@gmail.com>
2024-01-13 02:18:04 +0800

    Update for random French word generator

--- main.py
+++ main.py
@@ -2,39 +2,46 @@ import tkinter as tk
 import pandas
 import random
 BACKGROUND_COLOR = "#B1DDC6"
-window = tk.Tk()
-window.config(bg=BACKGROUND_COLOR)
-window.config(padx=50, pady=50)
-my_image = tk.PhotoImage(file="images/card_front.png")
-canvas = tk.Canvas(width=800, height=526, background=BACKGROUND_COLOR, highlightthickness=0)
-canvas.create_image(400, 260, image=my_image)
-canvas.grid(row=0, column=0, columnspan=2)
-
-# CANVAS TEXT
-canvas_language = canvas.create_text(400, 150, text="French", font=("Ariel", 40, "italic"))
-canvas_word = canvas.create_text(400, 263, text="Bonjour", font=("Ariel", 60, "bold"))
-
-# ..........................................WORD GENERATOR............................................
 
 french_raw_df = pandas.read_csv("data/french_words.csv")
 french_english_dict = french_raw_df.to_dict(orient="records")
 # print(french_english_dict[0]['French'])
-
+current_card = {}
 
 def generate_word():
+    global current_card
     current_card = random.choice(french_english_dict)
     canvas.itemconfig(canvas_language, text="French")
     canvas.itemconfig(canvas_word, text=current_card["French"])
+    window.after(3000, flip_card)
 
 
 
-# LABEL
+def flip_card():
+    canvas.itemconfig(canvas_img, image=new_image)
+    canvas.itemconfig(canvas_language, text="English", fill="white")
+    canvas.itemconfig(canvas_word, text=current_card["English"], fill="white")
 
-# language_label = tk.Label(text="French", font=("Ariel", 40, "italic"), background="white")
-# language_label.place(x=400, y=150, anchor="center")
-#
-# word_label = tk.Label(text="bonjour", font=("Ariel", 60, "bold"), background="white")
-# word_label.place(x=400, y=263, anchor="center")
+
+
+
+window = tk.Tk()
+window.config(bg=BACKGROUND_COLOR)
+window.config(padx=50, pady=50)
+old_image = tk.PhotoImage(file="images/card_front.png")
+new_image = tk.PhotoImage(file="images/card_back.png")
+canvas = tk.Canvas(width=800, height=526, background=BACKGROUND_COLOR, highlightthickness=0)
+canvas_img = canvas.create_image(400, 260, image=old_image)
+canvas.grid(row=0, column=0, columnspan=2)
+
+# CANVAS TEXT
+canvas_language = canvas.create_text(400, 150, text="Language", font=("Ariel", 40, "italic"))
+canvas_word = canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold"))
+
+window.after(5000, generate_word)
+
+
+
 
 
 # BUTTONS
